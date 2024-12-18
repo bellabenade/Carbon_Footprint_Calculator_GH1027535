8e0414615e6c18267213506be2e613514865975f
bellabenade <bellabenade19@gmail.com>
2024-12-18 22:40:23 +0100

    Recommendations

--- carbon_footprint_report.py
+++ carbon_footprint_report.py
@@ -5,7 +5,7 @@ from database import pie_chart, energy_bar_chart, \
     connection_creation, waste_bar_chart, \
     travel_bar_chart, bubble_chart, cf_calculation
 from pdf_generator import create_pdf
-from recommendations import recommendations
+from recommendations import data_tables
 
 
 def carbon_calculator():
@@ -35,31 +35,56 @@ def carbon_calculator():
     connection.close()
 
     if exist:
-        selection = st.selectbox('What would you like to see?', options = ['Recommendations', 'Energy Consumption', 'Waste Management', 'Fuel Consumption', 'How I compare to other companies', 'Download Report for This Year'])
+        selection = st.selectbox('What would you like to see?', options = ['Data and Recommendations', 'Energy Consumption', 'Waste Management', 'Fuel Consumption', 'How I compare to other companies', 'Download Report for This Year'])
 
-        if selection == 'Recommendations':
-            monthly_df, diff_html, total_change = recommendations(displayed_year)
+        if selection == 'Data and Recommendations':
+            monthly_df, diff_html, total_change, energy_con, waste_gen, travel_con, carbon_ft = data_tables(displayed_year)
             st.subheader('Your data for the past 12 months:')
+            st.text('This is the data that you provided us for the past 12 months.')
             st.table(monthly_df)
             st.subheader('Change in monthly consumption:')
+            st.text('This table shows how your consumption has changed from the previous month.')
             st.markdown(diff_html, unsafe_allow_html = True)
             st.subheader('Your total change in consumption over the last year:')
+            st.text("Here you can see your total change from the beginning until the end of the year. Let's see how you did!")
             st.markdown(total_change, unsafe_allow_html = True)
             st.subheader('You can consider the following recommendations:')
-
+            st.subheader('Recommendations:')
+            if energy_con > 0:
+                st.text('We see there has been an increase in your energy consumption. Maybe keep the following recommendations in mind:')
+                st.text('* Switch off all unnecessary appliances (in the office AND the lab) if it is not being used.')
+                st.text('* Consider utilizing environmentally friendly energy sources, such as solar power, if possible.')
+                st.text("* Don't let privileges , such as air-conditioning be abused by employees. Maybe crack a window in the summer time, in stead of constantly utilizing air-conditioning.")
+                st.text("* If it is possible, consider allowing employees to work from home occasionally to save on fuel costs and office space")
+            if waste_gen > 0:
+                st.text('We see there has been an increase in your waste generation. Maybe keep the following recommendations in mind:')
+                st.text('* Assess if there is more waste that could be recycled at your company. If you have chemical waste, you are welcome to contact us to dispose of your waste safely and economically!')
+                st.text('* Is it possible that some of your waste materials can be reused? It would not only be better for the environment, but can save you some industrial costs...')
+                st.text('* Identify what could have been a possible cause for the increase in waste generation. Was this only a once-off situation? If there is an increase every month, maybe investigate internally to identify what lead to this.')
+            if travel_con > 0:
+                st.text('We see there has been an increase in your energy consumption. Maybe keep the following recommendations in mind:')
+                st.text('*How efficient are your vehicles? It is always important to do regular maintenance on it and make sure that it is in optimal condition.')
+                st.text('*How fuel-efficient are your vehicles? Have you considered getting rid of vehicles with poor fuel efficiency and finding something that is more environmentally friendly?')
+                st.text('*Is there sufficient control over the use of company vehicles? Can your employees use the vehicles at their own discretion, or is there a process to approve the use thereof? If not, consider creating or updating your process.')
+                st.text('*If employees utilize their own vehicles, is there an approval process to manage vehicle usage for business purposes? If not, consider creating or updating your process.')
+                st.text('* If vehicles are rented, is the efficiency of the vehicle taken into account when selecting an option? Consider making this a part of the process.')
 
         elif selection == 'Energy Consumption':
             energy_bar = energy_bar_chart(displayed_year)
             st.plotly_chart(energy_bar)
+
         elif selection == 'Waste Management':
             waste_bar = waste_bar_chart(displayed_year)
             st.plotly_chart(waste_bar)
+
         elif selection == 'Fuel Consumption':
             travel_bar = travel_bar_chart(displayed_year)
             st.plotly_chart(travel_bar)
+
         elif selection == 'How I compare to other companies':
             cf_bubble = bubble_chart(displayed_year)
             st.plotly_chart(cf_bubble)
+
         elif selection == 'Download Report for This Year':
             pass
             # print('1')
--- recommendations.py
+++ recommendations.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 
-def recommendations(year):
+def data_tables(year):
     connection = connection_creation()
     cursor = connection.cursor()
     cursor.execute(f'''SELECT year, month, energy, waste, travel, carbon_footprint
@@ -22,34 +22,34 @@ def recommendations(year):
     monthly_data_df2['Monthly Change in Travel Consumption (%)'] = monthly_data_df2['Travel Consumption'].pct_change().fillna(0)*100
     monthly_data_df2['Monthly Change in Carbon Footprint (%)'] = monthly_data_df2['Carbon Footprint'].pct_change().fillna(0)*100
 
-    total_change = {'Year': '', 'Month': '',
-                    'Monthly Change in Energy Consumption (%)': monthly_data_df2['Monthly Change in Energy Consumption (%)'].sum(),
+    def fill_colour(table_value):
+        if table_value > 0:
+            return 'background-color: red'
+        elif table_value < 0:
+            return 'background-color: green'
+        else:
+            return 'background-color: blue'
+
+    monthly_difference_df = monthly_data_df2[
+        ['Year', 'Month', 'Monthly Change in Energy Consumption (%)', 'Monthly Change in Waste Generation (%)', 'Monthly Change in Travel Consumption (%)', 'Monthly Change in Carbon Footprint (%)']].drop(index = 0).astype(int)
+    monthly_difference_df = monthly_difference_df.style.applymap(fill_colour, subset = ['Monthly Change in Energy Consumption (%)', 'Monthly Change in Waste Generation (%)', 'Monthly Change in Travel Consumption (%)', 'Monthly Change in Carbon Footprint (%)'])
+    monthly_difference_df = monthly_difference_df.set_properties(**{'text-align': 'right'})
+
+    total_change = {'Monthly Change in Energy Consumption (%)': monthly_data_df2['Monthly Change in Energy Consumption (%)'].sum(),
                     'Monthly Change in Waste Generation (%)': monthly_data_df2['Monthly Change in Waste Generation (%)'].sum(),
                     'Monthly Change in Travel Consumption (%)': monthly_data_df2['Monthly Change in Travel Consumption (%)'].sum(),
                     'Monthly Change in Carbon Footprint (%)': monthly_data_df2['Monthly Change in Carbon Footprint (%)'].sum()}
 
+    total_change_df = pd.DataFrame(total_change, columns= ['Monthly Change in Energy Consumption (%)', 'Monthly Change in Waste Generation (%)', 'Monthly Change in Travel Consumption (%)', 'Monthly Change in Carbon Footprint (%)'],index = [0])
+    total_change_df = total_change_df.style.applymap(fill_colour).set_properties(**{'text-align': 'right'})
 
-    total_change_df = pd.DataFrame(total_change, index = [0])
-    print(total_change_df)
+    monthly_diff = monthly_difference_df.to_html()
+    total_change_html = total_change_df.to_html()
 
-    # def fill_colour(table_value):
-    #     if table_value > 0:
-    #         return 'background-color: red'
-    #     elif table_value < 0:
-    #         return 'background-color: green'
-    #     else:
-    #         return 'background-color: blue'
-    #
-    # monthly_difference_df = monthly_data_df2[
-    #     ['Year', 'Month', 'Monthly Change in Energy Consumption (%)', 'Monthly Change in Waste Generation (%)', 'Monthly Change in Travel Consumption (%)', 'Monthly Change in Carbon Footprint (%)']].drop(index = 0).astype(int)
-    # monthly_difference_df = monthly_difference_df.style.applymap(fill_colour, subset = ['Monthly Change in Energy Consumption (%)', 'Monthly Change in Waste Generation (%)', 'Monthly Change in Travel Consumption (%)', 'Monthly Change in Carbon Footprint (%)'])
-    # monthly_difference_df = monthly_difference_df.set_properties(**{'text-align': 'right'})
-    #
-    # #total_change_df = total_change_df.style.applymap(fill_colour).set_properties(**{'text-align': 'right'})
-    #
-    # monthly_diff = monthly_difference_df.to_html()
-    # #total_change = total_change_df.to_html()
+    energy_consumption = total_change['Monthly Change in Energy Consumption (%)']
+    waste_generation = total_change['Monthly Change in Waste Generation (%)']
+    travel_consumption = total_change['Monthly Change in Travel Consumption (%)']
+    carbon_footprint = total_change['Monthly Change in Carbon Footprint (%)']
 
-    return monthly_data_df1#, monthly_diff, total_change
+    return monthly_data_df1, monthly_diff, total_change_html, energy_consumption, waste_generation, travel_consumption, carbon_footprint
 
-recommendations(2024)
